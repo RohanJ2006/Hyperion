d1a6310e90efd0d7e16cb83d590d91ba907187d4
RohanJ2006 <devrohanj2006@gmail.com>
2026-03-12 13:33:03 +0000

    Created formulae.rs file
    Currently only contains the orbital_propagation() function. Implemented nalgebra for the Vector3 multiplication and other functions. Implemented serde for JSON deconstructing and reconstructing, might use simd-json later for speed. Needs a bit more optimisation down the line. Need to add more functions and bundle them together with the API flood.

---
+++ Backend/src/formulae.rs
@@ -0,0 +1,29 @@
+use crate::constants::{J2_PERTURBATION, RADIUS_OF_EARTH, STANDARD_GRAVITATIONAL_PARAMETER};
+use nalgebra::Vector3;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug, Clone, Copy)]
+pub struct StateVector {
+    pub position: Vector3<f64>, // Position vector for ECI in (km)
+    pub velocity: Vector3<f64>, // Velocity vector for ECI in (km/s)
+}
+
+pub fn orbital_propagation(position_vector: &Vector3<f64>) -> Vector3<f64> {
+    let position_norm = position_vector.norm();
+    // Magnitude of position vector
+    let position_norm_squared = position_norm.powi(2);
+    // Square of magnitude of position vector
+    let keplerian_orbit = -(STANDARD_GRAVITATIONAL_PARAMETER / (position_norm_squared * position_norm)) * position_vector;
+    // Two body value of keplerian orbit
+    let j2_left_coefficient = 1.5 * J2_PERTURBATION * STANDARD_GRAVITATIONAL_PARAMETER * RADIUS_OF_EARTH.powi(2) / position_norm.powi(5);
+    
+    let z_over_r = 5.0 * (position_vector.z.powi(2) / position_norm_squared);
+
+    let x_component = j2_left_coefficient * position_vector.x * (z_over_r - 1.0);
+    let y_component = j2_left_coefficient * position_vector.y * (z_over_r - 1.0);
+    let z_component = j2_left_coefficient * position_vector.z * (z_over_r - 3.0);
+
+    let j2_acceleration_vector = Vector3::new(x_component, y_component, z_component);
+
+    keplerian_orbit + j2_acceleration_vector
+}
