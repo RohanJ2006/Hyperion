aff010084d98909e99081586b14c268e2e96e023
Jeetu Purohit <jeetujpurohit@gmail.com>
2026-03-24 00:17:05 +0530

    Implemented the loader for the Web Assembly file
    Created a file that loads the WASM file ,
    create interfaces for exports and imports from the WASM ,
    get the reference to the shared memory at the same pointer mentioned in the WASM file

---
+++ frontend/src/dataPipeline/wasm_loader.ts
@@ -0,0 +1,43 @@
+export interface WasmExport {
+  memory : WebAssembly.Memory;
+  get_memory_pointer: ()=> number;
+  get_buffer_len: ()=> number;
+  compute_mercator: (count:number , width:number , height:number)=> void;
+}
+
+export interface WasmCore {
+ sharedMemory : Float64Array;
+ bufferLength : number;
+ stride : number;
+ computeMercator : (count : number , width : number , height : number) => void ;
+}
+
+export async function initWasmCore(): Promise<WasmCore> {
+  const response = await fetch('/math_engine.wasm');
+  const bytes = await response.arrayBuffer();
+
+  const { instance } = await WebAssembly.instantiate(bytes);
+
+  const exports = instance.exports as unknown as WasmExport;
+
+  const pointer = exports.get_memory_pointer();
+  const bufferLength = exports.get_buffer_len();
+
+  const sharedMemory = new Float64Array(
+    exports.memory.buffer,
+    pointer,
+    bufferLength
+  )
+
+  const stride = 6;
+
+  return {
+    sharedMemory,
+    bufferLength,
+    stride,
+    computeMercator:(count:number , width : number , height : number) => {
+      exports.compute_mercator(count , width , height) ;
+    },
+    
+  }
+}
