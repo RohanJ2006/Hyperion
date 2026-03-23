3ea64af77464db1e2ee13c638b1423946097c20d
Jeetu Purohit <jeetujpurohit@gmail.com>
2026-03-24 00:56:03 +0530

    implemented WASM in rust and websocket telemetry
    Wrote the Rust code that gets compiled to WASM file ,
    Implemented Websocket to get the high frequency data from the rust backend and store it in the shared memory for the WASM code to run on it

---
+++ frontend/src/dataPipeline/telemetryStream.ts
@@ -0,0 +1,61 @@
+export type telemetryCallback = (entityCount:number) => void;
+
+export interface telemetryStream {
+  connect: () => void; //connect the websocket
+  disconnect: () => void; //disconnect the websocket
+  isConnected:()=> boolean; //tells if connected or not 
+}
+
+export function initializeTelemetryStream (
+  sharedMemory : Float64Array,
+  stride : number,
+  onData : telemetryCallback
+):telemetryStream{
+  let ws : WebSocket | null = null;
+  let connected = false;
+
+  const floatsPerEntity = stride;
+
+  function connect():void{
+    if(ws) return ;
+    ws = new WebSocket('websocket.url'); // this needs to be changed with the url from the backend 
+    ws.binaryType = 'arraybuffer';
+
+    ws.onopen = ()=> {
+      connected = true;
+    };
+
+    ws.onmessage = (event:MessageEvent<ArrayBuffer>) => {
+      const incoming = new Float64Array(event.data);
+      const entityCount = (incoming.length / floatsPerEntity) | 0;
+
+      sharedMemory.set(incoming);
+
+      onData(entityCount);
+    };
+
+    ws.onclose = () => {
+      connected = false;
+      setTimeout(connect, 2000);
+    };
+
+    ws.onerror = () => {
+      ws?.close();
+    }
+  }
+
+  function disconnect(): void {
+    if(ws) {
+      ws.onclose = null;
+      ws.close();
+      ws = null;
+      connected = false;
+    }
+  }
+
+  function isConnected():boolean{
+    return connected;
+  }
+
+  return {connect , disconnect , isConnected} ;
+}
